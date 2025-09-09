a3c7f190162a2e76682de0078b556b6d8caed742
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-09 19:26:48 +0400

    A

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   output: "export",        // enable static export
   trailingSlash: true,
   basePath: "/enagram-test",
-  reactStrictMode: true,     // adds a trailing slash for GitHub Pages
+  reactStrictMode: true, 
+  assetPrefix: '/enagram-test/',
+    images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },  // adds a trailing slash for GitHub Pages
   // other config options if needed
 };
 
