c62800efb876aa4c57f0dce1172647ddce9f9a7e
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-09 22:36:28 +0400

    g

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/enagram-test',
+//   trailingSlash: true,
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/enagram-test',
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  ...(isProd
+    ? {
+        basePath: '/enagram-test',
+        assetPrefix: '/enagram-test/',
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
