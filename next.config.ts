ad0c801e4a4a42c3ffa0f4f85831353ea6048f60
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-09 19:18:24 +0400

    s

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",        // enable static export
-  trailingSlash: true,     // adds a trailing slash for GitHub Pages
+  trailingSlash: true,
+  basePath: "/enagram-test",
+  reactStrictMode: true,     // adds a trailing slash for GitHub Pages
   // other config options if needed
 };
 
