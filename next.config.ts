618d2567d3e363f6b2b639b42a6aef4aaa2c294d
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-09 19:34:56 +0400

    as

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",        // enable static export
   trailingSlash: true,
-  basePath: "/enagram-test",
-  reactStrictMode: true, 
-  assetPrefix: '/enagram-test/',
-    images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
-  },  // adds a trailing slash for GitHub Pages
-  // other config options if needed
-};
-
-module.exports = {
-  output: 'export',
-  basePath: '/enagram-test',
-  assetPrefix: '/enagram-test/',
+  reactStrictMode: true,
+  basePath: "/enagram-test",      // your GitHub repo name
+  assetPrefix: "/enagram-test/",  // ensures CSS/JS load correctly
   images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+    unoptimized: true,            // GitHub Pages doesn't support Next.js image optimization
   },
 };
 
